cbe2e1ef3adced48e058c49be1ab47ca7ce4340e
Kamil Żabiński <k.zabinski@peoplemore.pl>
2025-08-31 21:20:01 +0200

    Add examples

--- hs/src/HelVM/Hel/MiniHaskell/Classy.hs
+++ hs/src/HelVM/Hel/MiniHaskell/Classy.hs
@@ -10,7 +10,7 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-module HelVM.HelPS.MiniHaskell.Classy where
+module HelVM.Hel.MiniHaskell.Classy where
 import Prelude (Char, Int, String, succ)
 import Data.Char (chr, ord)
 import qualified Prelude
