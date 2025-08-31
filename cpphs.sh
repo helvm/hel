bd0ade4ed2b3e11f53ef7cd01a12175e4c10dc2f
Kamil Żabiński <k.zabinski@peoplemore.pl>
2025-08-31 21:09:18 +0200

    Add examples

--- cpphs.sh
+++ cpphs.sh
@@ -5,17 +5,17 @@ mkdir -p examples/mini-haskell/standalone/
 cpphs --noline --strip -DEMBEDDED   examples/mini-haskell/original/classy.hs > hs/src/HelVM/Hel/MiniHaskell/Classy.hs
 cpphs --noline --strip -DSTANDALONE examples/mini-haskell/original/classy.hs > examples/mini-haskell/standalone/classy.hs
 
-mkdir -p hs/src/HelVM/Hel/Compiler/Compiler/
+mkdir -p hs/src/HelVM/Hel/Compiler/
 mkdir -p examples/compiler/standalone/
 
-cpphs --noline --strip -DEMBEDDED   examples/compiler/original/typically.hs > hs/src/HelVM/Hel/Compiler/Compiler/Typically.hs
+cpphs --noline --strip -DEMBEDDED   examples/compiler/original/typically.hs > hs/src/HelVM/Hel/Compiler/Typically.hs
 cpphs --noline --strip -DSTANDALONE examples/compiler/original/typically.hs > examples/compiler/standalone/typically.hs
 
-cpphs --noline --strip -DEMBEDDED   examples/compiler/original/classy.hs > hs/src/HelVM/Hel/Compiler/Compiler/Classy.hs
+cpphs --noline --strip -DEMBEDDED   examples/compiler/original/classy.hs > hs/src/HelVM/Hel/Compiler/Classy.hs
 cpphs --noline --strip -DSTANDALONE examples/compiler/original/classy.hs > examples/compiler/standalone/classy.hs
 
-cpphs --noline --strip -DEMBEDDED   examples/compiler/original/barely.hs > hs/src/HelVM/Hel/Compiler/Compiler/Barely.hs
+cpphs --noline --strip -DEMBEDDED   examples/compiler/original/barely.hs > hs/src/HelVM/Hel/Compiler/Barely.hs
 cpphs --noline --strip -DSTANDALONE examples/compiler/original/barely.hs > examples/compiler/standalone/barely.hs
 
-cpphs --noline --strip -DEMBEDDED   examples/compiler/original/effectively.hs > hs/src/HelVM/Hel/Compiler/Compiler/Effectively.hs
+cpphs --noline --strip -DEMBEDDED   examples/compiler/original/effectively.hs > hs/src/HelVM/Hel/Compiler/Effectively.hs
 cpphs --noline --strip -DSTANDALONE examples/compiler/original/effectively.hs > examples/compiler/standalone/effectively.hs
